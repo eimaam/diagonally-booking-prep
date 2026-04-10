a74213dfb7db5261662099582781ef0936af9f7d
Imam Dahir <imamddahir@gmail.com>
2026-04-10 07:36:59 +0100

    add health check route

--- server/app.ts
+++ server/app.ts
@@ -1,6 +1,6 @@
 import cors from 'cors';
 import express, { type Request, type Response } from 'express';
-
+import profileRoutes from './routes/profile.routes';
 
 const app = express();
 const PORT = Number(process.env.PORT) || 8000;
@@ -8,6 +8,13 @@ const PORT = Number(process.env.PORT) || 8000;
 app.use(cors());
 app.use(express.json());
 
+// health check route
+app.get('/health', (_req: Request, res: Response) => {
+  res.json({
+    success: true,
+    message: 'diagonallyyyyyy',
+  });
+});
 
 
 app.listen(PORT, () => {
